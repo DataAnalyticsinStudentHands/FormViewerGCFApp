0095ac0bd30fa5ff6e2a739d9e3581bf496c2e89
Carl Steven <carl_steven@live.com>
2016-01-05 09:40:56 -0600

    clean add_plugin.js

--- hooks/before_platform_add/add_plugins.js
+++ hooks/before_platform_add/add_plugins.js
@@ -13,10 +13,7 @@ var pluginlist = [
 ];
 
 // no need to configure below
- 
-var fs = require('fs');
-var path = require('path');
-var sys = require('sys')
+var sys = require('sys');
 var exec = require('child_process').exec;
  
 function puts(error, stdout, stderr) {
